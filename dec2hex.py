f2f54e0382ceafd8586f118653da0b1b33779492
Stanciu14 <al425990@uji.es>
2022-09-29 18:25:39 +0200

    Update dec2hex.py

--- dec2hex.py
+++ dec2hex.py
@@ -69,8 +69,3 @@ if __name__ == "__main__":
 
 print("We love Pepe")
 print("We love Pepe")
-print("We love Pepe")
-print("We love Pepe")
-print("We love Pepe")
-print("We love Pepe")
-print("We love Pepe")
